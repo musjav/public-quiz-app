5cd20a783b811deb8e48a90c908808b9bb432083
musjav <mustafajaved751@gmail.com>
2024-03-23 02:39:46 +0500

    done

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { ref, set, getDatabase, push, onValue } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDQc3iaztIVvxhBt2EkGx-XHHH5SgeEbqE",
+    authDomain: "portofolio-c7959.firebaseapp.com",
+    databaseURL: "https://portofolio-c7959-default-rtdb.firebaseio.com",
+    projectId: "portofolio-c7959",
+    storageBucket: "portofolio-c7959.appspot.com",
+    messagingSenderId: "1005412350505",
+    appId: "1:1005412350505:web:af52f4d9b510fcb0347a38",
+    measurementId: "G-97401QYN86"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const database = getDatabase();
+
+
 var questions = [
     {
         question: "1. Html Stands For _______________________",
@@ -89,30 +115,102 @@ function showQuestion() {
         document.getElementById('restartBtn').style.display = 'block';
         // document.getElementById('nextBtn').style.display = 'none';
         // document.getElementById('prevBtn').style.display = 'none';
+        // Send the formatted percentage value to Firebase
+        // firebase.database().ref('path/to/your/data').set({
+        //     percentage: percentage
+        // }).then(() => {
+        //     console.log("Percentage value sent successfully to Firebase!");
+        // }).catch((error) => {
+        //     console.error("Error sending percentage value to Firebase:", error);
+        // });
+        endQuiz();
     }
 }
-showQuestion();
-function checkQues(a, correctAns) {
+// showQuestion();
+window.checkQues = function (a, correctAns) {
+    // if (a === correctAns) {
+    //     score++;
+    // }
+    // database.ref(questions.length).push({
+    //     question: questions[index].question,
+    //     options: questions[index].options,
+    //     correctAns: questions[index].correctAns
+    //   });
+
+    // index++;
+    // showQuestion();
     if (a === correctAns) {
         score++;
     }
+    const db = getDatabase();
+    const questionsRef = ref(db, 'questions');
+    push(questionsRef, {
+        question: questions[index].question,
+        options: questions[index].options,
+        correctAns: questions[index].correctAns
+    }).then(() => {
+        console.log("Question added to Firebase!");
+    }).catch((error) => {
+        console.error("Error adding question to Firebase:", error);
+    });
+
     index++;
     showQuestion();
 
 }
-function restartQuiz() {
+window.restartQuiz = function () {
+    // database.ref(questions.length).remove();
+
+    // document.getElementById('restartBtn').style.display = 'none';
+
+    // index = 0;
+    // score = 0;
+    // document.getElementById('restartBtn').style.display = 'none';
+    // showQuestion();
+    const db = getDatabase();
+    const questionsRef = ref(db, 'questions');
+    set(questionsRef, null).then(() => {
+        console.log("Questions cleared from Firebase!");
+    }).catch((error) => {
+        console.error("Error clearing questions from Firebase:", error);
+    });
+
+    document.getElementById('restartBtn').style.display = 'none';
+
     index = 0;
     score = 0;
-    document.getElementById('restartBtn').style.display = 'none';
     showQuestion();
-    // console.log(index,score)
-    // Enable the "Next" button by default
-    // document.getElementById('nextBtn').style.display = 'block';
-    // Hide the "Previous" button when restarting
-    // document.getElementById('prevBtn').style.display = 'block';
-    // Hide the "Restart" button when restarting
-    // Display the first question
 }
+window.sendPercentageToFirebase = function (percentage) {
+    // Get a reference to the root of the database
+    const dbRef = getDatabase();
+
+    // Push the percentage value to a new child node under "Tasks"
+    const tasksRef = ref(dbRef, 'Tasks');
+    push(tasksRef, {
+        percentage: percentage
+    }).then(() => {
+        console.log("Percentage value sent successfully to Firebase!");
+    }).catch((error) => {
+        console.error("Error sending percentage value to Firebase:", error);
+    });
+}
+
+
+window.endQuiz = function () {
+    // Calculate percentage
+    var percentage = (score / totalQuestions) * 100;
+    percentage = percentage.toFixed(2);
+
+    // Display quiz result
+    ques.innerHTML = `Quiz Ended! Your Score: ${score}/${totalQuestions} (${percentage}%)`;
+    document.getElementById('restartBtn').style.display = 'block';
+
+    // Send percentage to Firebase
+    sendPercentageToFirebase(percentage);
+}
+
+showQuestion();
 // // //================================================================FOR MAKING NEXT & PREVIOUS BUTTON UNCOMMENT ALL COMMENTS==============================================================
 // // // ==========================================================PREVIOUS BUTTON
 // function previousQuestion() {
